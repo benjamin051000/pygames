47cb74467a0364e3a326f9ea8b7a14252506677e
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2021-08-01 12:27:25 -0400

    added + operator

--- snake/colors.py
+++ snake/colors.py
@@ -13,3 +13,6 @@ class Colors(Enum):
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
+
+    def __add__(self, other):
+        return (self.value[0] + other[0], self.value[1] + other[1], self.value[2] + other[2])
